fffe94cfd46683677a5df3b7d66981bc08e7a080
Prince-1307 <230210116057@gtu.edu.in>
2026-06-13 15:58:37 +0530

    Added Flask templates and landing page

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
+from routes.home import home_bp
+
 app = Flask(__name__)
 
-
-@app.route("/")
-def home():
-    return "Interview Intelligence Platform"
+app.register_blueprint(home_bp)
 
 
 if __name__ == "__main__":
+
     app.run(debug=True)
